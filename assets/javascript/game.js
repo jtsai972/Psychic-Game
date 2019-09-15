5d2483b5cc2726bff93e14fb8ed3eff4aa98ff77
J Tsai <jtsai972@gmail.com>
2019-09-15 15:44:19 -0500

    finishing basic code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,17 @@
 //Basic Variables
 let countWin, countLoss, countGuess, letter, guess;
 
+// Initializing variables
+countWin = countLoss = 0;
+countGuess = 9;
+guess = " ";
+
+let guessHistory = document.getElementById("guessHistory");
+
+var rndGen = () => {
+    letter = String.fromCharCode(97+Math.floor(Math.random() * 26));
+}
+
 //Printing function
 var print = function() {
     //Setting scores
@@ -10,29 +21,55 @@ var print = function() {
     document.getElementById("guessCount").innerHTML= countGuess;
 
     //Printing guesses
-    document.getElementById("guessHistory").innerHTML += " " + guess;
+    guessHistory.innerHTML += guess;
 }
+
 var check = function() {
     if (letter== guess) {
+        //add a win
         countWin++;
-    } else {
-        countLoss++;
+
+        alert("Congratulations! You guessed correctly!") ;
+
+        //select a new letter
+        reset();        
     }
 
-    if (countGuess == 0) {
+    if (countGuess <= 0) {
+        //add a loss
+        countLoss++;
         
+        //reset info
+        reset();
     }
 }
 
-// Initializing variables
-countWin = countLoss = 0;
-countGuess = 9;
-letter = String.fromCharCode(97+Math.floor(Math.random() * 26));
+var reset = function() {
+    guess = " ";
+    countGuess= 9;
+    rndGen();
+    guessHistory.innerHTML = " ";
+
+    //checking for letter
+    console.log(letter); 
+}
+
+rndGen();
+
+print();
 
 //checking for letter
 console.log(letter);
 
-check();
+document.addEventListener(
+    'keypress', function(event) {
+        //setting guess for comparison
+        guess = String.fromCharCode(event.keyCode).toLowerCase();
 
-// Printing results
-print();
+        //counting guesses
+        countGuess--;
+
+        check(); //check what is pressed
+        print (); //print result
+    }
+);
