e975574c90073ea2bb38197fba4d6d79a17bb160
J Tsai <jtsai972@gmail.com>
2019-09-15 13:28:20 -0500

    repositioning elements and setting up a bit more javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,27 @@
 
 //Basic Variables
-let countWin, countLoss, countGuess, letter, guesses;
+let countWin, countLoss, countGuess, letter, guess;
 
 //Printing function
 var print = function() {
+    //Setting scores
     document.getElementById("wins").innerHTML = countWin;
     document.getElementById("losses").innerHTML = countLoss;
     document.getElementById("guessCount").innerHTML= countGuess;
+
+    //Printing guesses
+    document.getElementById("guessHistory").innerHTML += " " + guess;
 }
 var check = function() {
-    
+    if (letter== guess) {
+        countWin++;
+    } else {
+        countLoss++;
+    }
+
+    if (countGuess == 0) {
+        
+    }
 }
 
 // Initializing variables
@@ -19,6 +31,8 @@ letter = String.fromCharCode(97+Math.floor(Math.random() * 26));
 
 //checking for letter
 console.log(letter);
+
+check();
 
 // Printing results
 print();
